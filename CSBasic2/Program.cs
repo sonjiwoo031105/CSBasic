850670c51424ad21c63864f178d13837728a15d4
ziu <57125940+sonjiwoo031105@users.noreply.github.com>
2021-05-04 12:19:57 +0900

    #15 제어문 응용 실습

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -98,10 +98,23 @@ namespace CSBasic2
 
             Console.WriteLine(number > 0 ? "자연수입니다." : "자연수가 아닙니다.");
             //Console.WriteLine((number > 0)? a() : b());
+
+            Console.Write("입력: ");
+            string line = Console.ReadLine();
+            if (line.Contains("안녕"))
+            {
+                Console.WriteLine("너도 안녕~");
+            }else if (line.Contains("바보"))
+            {
+                Console.WriteLine("너가 바보야!");
             }
-
-        
-          
-
+            else if (line.Contains("밥"))
+            {
+                Console.WriteLine("배고파~");
+            }
+            else
+                Console.WriteLine("심심해");
+            
+        }
     }
 }
