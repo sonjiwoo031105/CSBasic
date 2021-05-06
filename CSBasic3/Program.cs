2db14e7903fea91e5ba8ac7488275bfd99eb19d2
ziu <57125940+sonjiwoo031105@users.noreply.github.com>
2021-05-06 10:07:56 +0900

    #18 while / do while 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -17,7 +17,20 @@ namespace CSBasic3
             //Console.WriteLine(intArray[100]); //범위 초과
             //Console.WriteLine(intArray[-1]); // 음수 사용 불가
 
+            int i = 0;
+            int[] intArray2 = { 52, 273, 32, 65, 103 };
+            while(i < intArray.Length)
+            {
+                Console.WriteLine(i + "번째 출력:" + intArray2[i]);
+                i++;
+            }
 
+            string input;
+            do
+            {
+                Console.Write("입력(종료:exit):");
+                input = Console.ReadLine();
+            } while (input != "exit");
         }
     }
 }
