65f1fa5226bfc97cf248b4b31ca2020ecbb2bfba
ziu <57125940+sonjiwoo031105@users.noreply.github.com>
2021-05-06 09:58:48 +0900

    #16 배열 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -10,6 +10,12 @@ namespace CSBasic3
     {
         static void Main(string[] args)
         {
+            int[] intArray = new int[100];
+            Console.WriteLine(intArray.Length);
+            Console.WriteLine(intArray[0]);
+            Console.WriteLine(intArray[99]);
+            //Console.WriteLine(intArray[100]); //범위 초과
+            //Console.WriteLine(intArray[-1]); // 음수 사용 불가
         }
     }
 }
