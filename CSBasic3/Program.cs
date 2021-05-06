3c477ab3c37e3724fe60fe4e7241183106e5196b
ziu <57125940+sonjiwoo031105@users.noreply.github.com>
2021-05-06 10:19:49 +0900

    #19 for문 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -19,7 +19,7 @@ namespace CSBasic3
 
             int i = 0;
             int[] intArray2 = { 52, 273, 32, 65, 103 };
-            while(i < intArray.Length)
+            while(i < intArray2.Length)
             {
                 Console.WriteLine(i + "번째 출력:" + intArray2[i]);
                 i++;
@@ -31,6 +31,11 @@ namespace CSBasic3
                 Console.Write("입력(종료:exit):");
                 input = Console.ReadLine();
             } while (input != "exit");
+
+            for(int j = '가'; j <= '힣'; j++)
+            {
+                Console.Write((char)j);
+            }
         }
     }
 }
