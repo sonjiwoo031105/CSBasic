8929af9ff1eebf2382f407af768caec2334b3b4e
ziu <57125940+sonjiwoo031105@users.noreply.github.com>
2021-05-27 10:20:31 +0900

    #23 클래스 변수 실습

--- CSBasic4/Program.cs
+++ CSBasic4/Program.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace CSBasic4
 {
+    class MyMath
+    {
+        public static double PI = 3.141592;
+        public static void Greeting()
+        {
+           // Console.WriteLine(somenumber);
+            Console.WriteLine("Greeting!");
+        }
+    }
     class Car
     {
         int carNumber;
@@ -38,7 +47,7 @@ namespace CSBasic4
             Random random = new Random();
             Console.WriteLine(random.Next());
             Console.WriteLine(random.Next(100));
-            Console.WriteLine(random.Next(1,19));
+            Console.WriteLine(random.Next(1, 19));
             Console.WriteLine(random.NextDouble());
             Console.WriteLine(random.NextDouble());
             Console.WriteLine(random.NextDouble());
@@ -50,7 +59,7 @@ namespace CSBasic4
             foreach (var item in list)
             {
                 Console.WriteLine("Count: " + list.Count + "\titem:" + item);
-                list.Remove(item);
+                //list.Remove(item);
             }
 
             Console.WriteLine(Math.Abs(-52273));
@@ -60,6 +69,20 @@ namespace CSBasic4
             Console.WriteLine(Math.Round(52.563));
             Console.WriteLine(Math.Max(52, 273));
             Console.WriteLine(Math.Min(52, 273));
+
+            Product product = new Product();
+            product.name = "감자";
+            product.price = 2000;
+
+            Console.WriteLine(product.name + ": " + product.price + "원");
+
+            Product productA = new Product() { name = "짜장면", price = 5500 };
+            Product productB = new Product() { name = "짬봉", price = 8000 };
+            Product productC = new Product() { name = "탕수육" };
+            Product productD = new Product() { price = 9999999 };
+            Product productE = new Product() { price = 30000, name = "양장피" };
+
+
 
 
 
