fcf286992b7d577541e9254047ecc36b36d13a52
ziu <57125940+sonjiwoo031105@users.noreply.github.com>
2021-05-27 09:49:44 +0900

    #22 클래스 개요, 사용, 생성 실습

--- CSBasic4/Program.cs
+++ CSBasic4/Program.cs
@@ -6,10 +6,63 @@ using System.Threading.Tasks;
 
 namespace CSBasic4
 {
+    class Car
+    {
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+        public void SetOutTime()
+        {
+            this.outTime = DateTime.Now;
+        }
+    }
     class Program
     {
+        class FirstClass
+        {
+
+        }
+        class SecondClass
+        {
+
+        }
         static void Main(string[] args)
         {
+            Car[] cara = new Car[10];
+
+            Random random = new Random();
+            Console.WriteLine(random.Next());
+            Console.WriteLine(random.Next(100));
+            Console.WriteLine(random.Next(1,19));
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+
+            List<int> list = new List<int>() { 52, 273, 32, 64 };
+
+            foreach (var item in list)
+            {
+                Console.WriteLine("Count: " + list.Count + "\titem:" + item);
+                list.Remove(item);
+            }
+
+            Console.WriteLine(Math.Abs(-52273));
+            Console.WriteLine(Math.Ceiling(52.273));
+            Console.WriteLine(Math.Floor(52.273));
+            Console.WriteLine(Math.Round(52.263));
+            Console.WriteLine(Math.Round(52.563));
+            Console.WriteLine(Math.Max(52, 273));
+            Console.WriteLine(Math.Min(52, 273));
+
+
+
         }
     }
 }
