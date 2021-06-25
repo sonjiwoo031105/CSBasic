cf8ed0b52bed794e5dc541eb7329bc1c6b45041b
Jiwoo Son <jiji432@naver.com>
2021-06-25 12:15:14 +0900

    #26 메서드 기본

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -8,11 +8,36 @@ namespace CSBasic5
 {
     class Program
     {
+        class MethodExample
+        {
+            public int Power(int x)
+            {
+                return x * x;
+            }
+
+            public int Multi(int x, int y)
+            {
+                return x * y;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("Print() 메서드 호출됨");
+            }
+        }
         static void Main(string[] args)
         {
             Car car = new Car();
             car.SetInTime();
             car.SetOutTime();
+
+
+            MethodExample me = new MethodExample();
+            Console.WriteLine(me.Power(10));
+            Console.WriteLine(me.Power(20));
+            Console.WriteLine(me.Multi(52, 273));
+            Console.WriteLine(me.Multi(103, 32));
+            me.Print();
         }
     }
 }
